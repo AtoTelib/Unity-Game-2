d46864e2c21bb3219883a21d1bba92334fb5e61a
MohammadAshraf10 <mohammadashraf_10@hotmail.com>
2020-12-13 00:17:42 +0200

    workingDialouges

---
+++ Assets/Scripts/DialougeScripts/DialougeManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialougeText;
+    public Animator animator;
+   
+    public static bool grannyInteract=false;
+private  Queue<string> sentences;
+    void Start()
+    {
+        sentences=new Queue<string>();  
+    }
+    public void StartDialouge(Dialouge dialouge){
+
+        animator.SetBool("isOpen",true);
+        grannyInteract=true;
+     //   
+
+        nameText.text=dialouge.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialouge.sentences){
+            sentences.Enqueue(sentence);
+            
+        }
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence(){
+        if(sentences.Count==0){
+            EndDialouge();
+            return;
+        }
+        string sentence=sentences.Dequeue();
+        dialougeText.text=sentence;
+    }
+    public void EndDialouge(){
+        animator.SetBool("isOpen",false);
+    //    grannyAnimator.SetBool("interact",false);
+        grannyInteract=false;
+
+
+    }
+
+  
+}
---
+++ Assets/Scripts/GrannyAnimations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrannyAnimations : MonoBehaviour
+{
+     public Animator grannyAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DialougeManager.grannyInteract){
+            grannyAnimator.SetBool("interact",true);
+        }else {
+            grannyAnimator.SetBool("interact",false);
+        }
+        
+    }
+   
+}
