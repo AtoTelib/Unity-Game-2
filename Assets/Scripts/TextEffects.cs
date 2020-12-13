c643072fd18f171dd897a1dc6c894acb56fa8337
zainab <zainabsultan1111@gmail.com>
2020-12-13 09:07:37 +0200

    added ui and texture

--- Assets/Scripts/TextEffects.cs
+++ Assets/Scripts/TextEffects.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System;
 public class TextEffects : MonoBehaviour
 {
     //Fade time in seconds
          public float fadeOutTime;
          public Text text;
          public float alpha;
+         bool brighten;
     // Start is called before the first frame update
+    public bool flashing;
     void Start()
     {
 
         text = GetComponent<Text>();
-        alpha =0.0f;
+        if(!flashing)
+            alpha =0.0f;
+        else
+            alpha=1.0f;
+        brighten= false;
 
         
     }
@@ -22,18 +28,45 @@ public class TextEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
 
         
-        if(alpha<1.0f){
+        if(alpha<1.0f || flashing){
             Color originalColor = text.color;
 
              for (float t = 0.01f; t < fadeOutTime; t += Time.deltaTime)
              {
                  //text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t/fadeOutTime));
                  //Color originalColor  = text.color;
-                 originalColor.a = alpha+0.00001f;
-                 alpha = alpha+0.00001f;
+                 if(flashing){
+                     if(brighten){
+                        originalColor.a = alpha+0.000004f;
+                        alpha = alpha+0.000004f;
+                        if(Math.Abs(alpha-1.0)<0.01){
+                            brighten=false;
+
+                     }
+
+                     }
+                     
+                     else{
+                        originalColor.a = alpha-0.000004f;
+                        alpha = alpha-0.000004f;
+                        if(Math.Abs(alpha-0)<0.01){
+                            brighten=true;
+                        }
+
+                     }
+                     text.color = originalColor;
+                 }
+
+
+                 else{
+                 originalColor.a = alpha+0.000003f;
+                 alpha = alpha+0.000003f;
                  text.color = originalColor;
+                 }
+                 
                  
              }
         }
